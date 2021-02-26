3dbf418521852150bd4c13a9f7f3ca8967ea95e7
Elio Struyf <elio@struyfconsulting.be>
2021-02-26 10:32:54 +0100

    Implemented retry + fix in cypress for setting variables

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,9 +10,9 @@ interface config {
  */
 function getConfig(): config {
   return {
-    username: !process.env.CI ? Cypress.env('USERNAME') : process.env.USERNAME,
-    password: !process.env.CI ? Cypress.env('PASSWORD') : process.env.PASSWORD,
-    siteUrl: !process.env.CI ? Cypress.env('SITEURL') : process.env.SITEURL,
+    username: process.env.USERNAME || Cypress.env('USERNAME'),
+    password: process.env.PASSWORD || Cypress.env('PASSWORD'),
+    siteUrl: process.env.SITEURL || Cypress.env('SITEURL'),
   };
 }
 
@@ -21,7 +21,6 @@ function getConfig(): config {
  */
 Cypress.Commands.add("visitSP", (pageUrl: string | null) => {
   const config = getConfig();
-  console.log(config)
   cy.task('NodeAuth', config).then((data: any) => {
     cy.visit(`${config.siteUrl}/${pageUrl}`, {
       headers: data.headers,
