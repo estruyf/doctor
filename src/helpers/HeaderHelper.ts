2ea8fe52e59699ad5463e0210f2011eb00930942
Elio Struyf <elio@struyfconsulting.be>
2021-02-26 08:38:34 +0100

    Fix set header

--- src/helpers/HeaderHelper.ts
+++ src/helpers/HeaderHelper.ts
@@ -40,9 +40,9 @@ export class HeaderHelper {
 
         // Start folder creation process
         crntFolder = await FolderHelpers.create(crntFolder, folders, webUrl);
-        const absImgUrl = await FileHelpers.create(crntFolder, imgPath, webUrl, overwriteImages);
+        await FileHelpers.create(crntFolder, imgPath, webUrl, overwriteImages);
 
-        const imgUrl = FileHelpers.getRelUrl(webUrl, `${absImgUrl}`);
+        const imgUrl = FileHelpers.getRelUrl(webUrl, `${crntFolder}/${path.basename(header.image)}`);
         setPageHeader = `${setPageHeader} --imageUrl "${imgUrl}"`;
 
         if (header.altText) {
