a56d5c0babef51c1341876ac83f914083ad8a306
Elio Struyf <elio@struyfconsulting.be>
2021-03-04 10:13:28 +0100

    #71 - Added easier debug configuration

---
+++ scripts/init-debug.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const doctorFile = path.join(__dirname, `../bin/doctor`);
+const doctorContents = fs.readFileSync(doctorFile, { encoding: "utf-8" });
+
+const lines = doctorContents.split(`\n`);
+lines[0] = `#!/usr/bin/env node --inspect`;
+
+fs.writeFileSync(doctorFile, lines.join(`\n`));
