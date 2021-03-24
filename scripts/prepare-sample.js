c6386e578e65a4e83af74d48646bcd6d15e679e6
Elio Struyf <elio@struyfconsulting.be>
2021-03-24 13:59:00 +0100

    Update for sample doctor.json

--- scripts/prepare-sample.js
+++ scripts/prepare-sample.js
@@ -4,3 +4,12 @@ const path = require('path');
 const config = path.join(__dirname, `../doctor-sample/doctor.sample.json`);
 const newPath = path.join(__dirname, `../doctor-sample/doctor.json`);
 fs.copyFileSync(config, newPath);
+
+const file = fs.readFileSync(newPath, { encoding: "utf-8" });
+if (file) {
+  const data = JSON.parse(file);
+  if (data && data.multilingual && data.multilingual.translator) {
+    data.multilingual.translator.key = process.env.TRANSLATOR_KEY;
+    fs.writeFileSync(newPath, JSON.stringify(data, null, 2), { encoding: "utf-8" });
+  }
+}
